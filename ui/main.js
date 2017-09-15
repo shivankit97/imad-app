9de09a93ee824b1fcf80e8a9dd91d2203ef5b1ab
shivankit97 <30933226+shivankit97@users.noreply.github.com>
2017-09-15 15:26:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var bob=document.getElementById('abc');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    bob.style.marginLeft=marginleft+"px";
+}
+bob.onclick=function(){
+    var interval=setInterval(moveright,100);
+};
