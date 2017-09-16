087ca693367c32b6816cd1a7954987f26786edc6
shivankit97 <30933226+shivankit97@users.noreply.github.com>
2017-09-16 17:19:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,18 @@
-console.log('Loaded!');
-var bob=document.getElementById('abc');
-var marginleft=0;
-function moveright()
-{
-    marginleft=marginleft+10;
-    bob.style.marginLeft=marginleft+"px";
-}
-bob.onclick=function(){
-    var interval=setInterval(moveright,100);
+
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            
+            if(request.status==200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://http://shivankitr97.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
