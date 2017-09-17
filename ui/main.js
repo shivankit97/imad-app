c9a04032242552b4787dbd1b58cf5987fb2c3bf8
shivankit97 <30933226+shivankit97@users.noreply.github.com>
2017-09-17 14:31:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,4 +15,18 @@ button.onclick=function(){
     };
     request.open('GET','http://shivankitr97.imad.hasura-app.io/counter',true);
     request.send(null);
+};    
+var nameinput=document.getElementById('name');
+var namei =nameinput.value;
+var submit=document.getElementById('submit_id');
+submit.onclick=function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list +='<li>'+names[i]+'</li>';
+    }
+   var ul=document.getElementById('namelist');
+   ul.innerHTML=list;
 };
+
+    
