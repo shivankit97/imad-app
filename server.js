c0a731f68d585b9648b983d1ca4a690aa2751767
shivankit97 <30933226+shivankit97@users.noreply.github.com>
2017-09-17 15:02:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,13 @@ app.get('/counter',function(req,res){
    res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name',function(req,res){    //submit-name?name==xxxxx
+   var name=req.query.name; 
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
+
 app.get('/:articlename', function(req, res){
     var articlename= req.params.articlename;
   res.send(createtemplate(articles[articlename]));
