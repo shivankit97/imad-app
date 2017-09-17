88027d4a460fc452656a60c4d5df7c22c3550ffc
shivankit97 <30933226+shivankit97@users.noreply.github.com>
2017-09-17 15:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,13 +20,25 @@ var nameinput=document.getElementById('name');
 var namei =nameinput.value;
 var submit=document.getElementById('submit_id');
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list +='<li>'+names[i]+'</li>';
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            
+            if(request.status==200){
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+            list +='<li>'+names[i]+'</li>';
     }
    var ul=document.getElementById('namelist');
    ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET','http://shivankitr97.imad.hasura-app.io/submit-name?name='+ namei,true);
+    request.send(null);
 };
 
     
