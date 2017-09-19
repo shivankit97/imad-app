4a7a22b25cf66e1f6e3c1dc0bbee29d7a59cf830
shivankit97 <30933226+shivankit97@users.noreply.github.com>
2017-09-19 16:53:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/:articlename', function(req, res){
     var articlename= req.params.articlename;
   res.send(createtemplate(articles[articlename]));
 });
-var pool = new Pool('config');
+var pool = new pool('config');
 app.get('/test-db', function(req,res){
         pool.query('SELECT * FROM test', function(err,result){
             if(err){
